c6d657532be7bae06210e769377ca0530a45eec3
Manas Singh <anshumaanvsingh@gmail.com>
2026-05-03 03:24:29 -0400

    Initial Commit

---
+++ Idionous/IdionousApp.swift
@@ -0,0 +1,17 @@
+//
+//  IdionousApp.swift
+//  Idionous
+//
+//  Created by Manas Singh on 5/3/26.
+//
+
+import SwiftUI
+
+@main
+struct IdionousApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
